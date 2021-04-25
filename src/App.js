a4387c2c39b4cadd694414bfcb2a03b8252a526e
Eduardo Fischer <efischer@outlook.com.br>
2021-04-25 17:13:26 -0300

    API Fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [nomes, setNomes] = useState([])
+  const [cartoes, setCartoes] = useState([])
+
+  useEffect(() => {
+    console.debug('effect')
+    axios.get('http://3.216.15.43:8000/api/grupo')
+      .then(res => {
+        console.debug(res)
+        if (res?.data?.nomes)
+          setNomes(res.data.nomes)
+        if (res?.data?.cartoes)
+          setCartoes(res.data.cartoes)
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +29,17 @@ function App() {
         <p>
           App AP2 PDP
         </p>
+        <p>Os dados abaixo foram consultados de uma API rodando em NodeJs hospedada em uma máquina virtual rodando na AWS.</p>
+        
+        <h4>Componentes do Grupo</h4>
+        <div style={{display: 'flex'}}>
+          <div style={{display: 'flex', flexDirection: 'column', marginRight: '10px'}}>
+            {nomes.map(nome => <div key={nome}>{nome}</div>)}
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            {cartoes.map(cartao => <div key={cartao}>{cartao}</div>)}
+          </div>
+        </div>
       </header>
     </div>
   );
